a827ece5b74ab3fc374e6d6c677215643987cf93
msilvadoug <msilva.doug@gmail.com>
2017-12-07 00:01:23 -0200

    Counting points with the collision of the cubes with the balls
    Counting points with the collision of the cubes with the balls
    
    The number of points can be set individually for each cube, I have created a public class to be easy to define how many points each cube will give.

---
+++ Assets/Scripts/CollisionBall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBall : MonoBehaviour {
+
+	// Use this for initialization
+
+	Player player;
+	public int  points;
+
+	void Start () {
+		player = FindObjectOfType<Player> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		
+		//Check for a match with the specified name on any GameObject that collides with your GameObject
+		if (collision.gameObject.name == "Sphere")
+		{
+			
+			player.points += points;
+			Destroy(gameObject);
+
+		}
+			
+}
+
+
+
+
+}
---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour {
+
+	Player player;
+	public Text pointstext;
+
+	// Use this for initialization
+	void Start () {
+
+		player = FindObjectOfType<Player> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		pointstext.text = ("Points: " + player.points);
+		
+	}
+}
